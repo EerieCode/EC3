4ca3ea6acb3b00992ecb708fce78681e2af4c2b3
EerieCode <eeriecode@gmail.com>
2016-01-12 20:10:24 +0100

    Update and rename c700666000.lua to c216666000.lua

--- script/c216666000.lua
+++ script/c216666000.lua
@@ -1,34 +1,34 @@
 --Created and scripted by Eerie Code
 --The Phantom Knights of Torn Banner
-function c700666000.initial_effect(c)
+function c216666000.initial_effect(c)
   --Activate effect
   --Phantom Xyz
   local e2=Effect.CreateEffect(c)
   e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
   e2:SetType(EFFECT_TYPE_IGNITION)
   e2:SetRange(LOCATION_RANGE)
-  e2:SetCountLimit(1,700666000+1)
-  e2:SetTarget(c700666000.sptg)
-  e2:SetOperation(c700666000.spop)
+  e2:SetCountLimit(1,216666000+1)
+  e2:SetTarget(c216666000.sptg)
+  e2:SetOperation(c216666000.spop)
   c:RegisterEffect(e2)
 end
 
-function c700666000.mfilter(c)
+function c216666000.mfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0xdb) and not c:IsType(TYPE_TOKEN+TYPE_XYZ)
 end
-function c700666000.xyzfilter(c,mg)
+function c216666000.xyzfilter(c,mg)
 	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsXyzSummonable(mg)
 end
-function c700666000.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c216666000.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local g=Duel.GetMatchingGroup(c700666000.mfilter,tp,LOCATION_MZONE,0,nil)
-		return Duel.IsExistingMatchingCard(c700666000.xyzfilter,tp,LOCATION_DECK,0,1,nil,g)
+		local g=Duel.GetMatchingGroup(c216666000.mfilter,tp,LOCATION_MZONE,0,nil)
+		return Duel.IsExistingMatchingCard(c216666000.xyzfilter,tp,LOCATION_DECK,0,1,nil,g)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c700666000.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c700666000.mfilter,tp,LOCATION_MZONE,0,nil)
-	local xyzg=Duel.GetMatchingGroup(c700666000.xyzfilter,tp,LOCATION_DECK,0,nil,g)
+function c216666000.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c216666000.mfilter,tp,LOCATION_MZONE,0,nil)
+	local xyzg=Duel.GetMatchingGroup(c216666000.xyzfilter,tp,LOCATION_DECK,0,nil,g)
 	if xyzg:GetCount()>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
