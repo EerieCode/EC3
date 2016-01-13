4e1e658dabd12bddf39eb421224a5b255eb62312
EerieCode <eeriecode@gmail.com>
2016-01-13 22:10:51 +0100

    Create c216666002.lua

---
+++ script/c216666002.lua
@@ -0,0 +1,221 @@
+--Created and scripted by Eerie Code
+--The Phantom Knights of Fragmented Ring
+function c216666002.initial_effect(c)
+    --Pendulum
+    aux.EnablePendulumAttribute(c)
+    --Increase ATK
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_PZONE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x10db))
+    e1:SetValue(300)
+    c:RegisterEffect(e1)
+    --Phantom Pendulum
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_PZONE)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetCountLimit(1)
+    e2:SetCondition(c216666002.pncon)
+    e2:SetTarget(c216666002.pntg)
+    e2:SetOperation(c216666002.pnop)
+    c:RegisterEffect(e2)
+    --equip
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(42940404,0))
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetCategory(CATEGORY_EQUIP)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetTarget(c216666002.eqtg)
+    e3:SetOperation(c216666002.eqop)
+    c:RegisterEffect(e3)
+    --unequip
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(42940404,1))
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_SZONE)
+    e4:SetCondition(c216666002.uncon)
+    e4:SetTarget(c216666002.sptg)
+    e4:SetOperation(c216666002.spop)
+    c:RegisterEffect(e4)
+    --destroy sub
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_EQUIP)
+    e5:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e5:SetCode(EFFECT_DESTROY_SUBSTITUTE)
+    e5:SetCondition(c216666002.uncon)
+    e5:SetValue(1)
+    c:RegisterEffect(e5)
+    --eqlimit
+    local e6=Effect.CreateEffect(c)
+    e6:SetType(EFFECT_TYPE_SINGLE)
+    e6:SetCode(EFFECT_EQUIP_LIMIT)
+    e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e6:SetValue(aux.TargetBoolFunction(Card.IsSetCard,0x10db))
+    c:RegisterEffect(e6)
+    --atk up
+    local e7=Effect.CreateEffect(c)
+    e7:SetType(EFFECT_TYPE_EQUIP)
+    e7:SetCode(EFFECT_UPDATE_ATTACK)
+    e7:SetValue(600)
+    e7:SetCondition(c216666002.uncon)
+    c:RegisterEffect(e7)
+    --Double Attack
+    local e8=Effect.CreateEffect(c)
+    e8:SetType(EFFECT_TYPE_EQUIP)
+    e8:SetCode(EFFECT_EXTRA_ATTACK)
+    e8:SetValue(1)
+    e8:SetCondition(c216666002.uncon)
+    c:RegisterEffect(e8)
+    --ATK Up
+    local e9=Effect.CreateEffect(c)
+    e9:SetType(EFFECT_TYPE_QUICK_O)
+    e9:SetCode(EVENT_FREE_CHAIN)
+    e9:SetRange(LOCATION_GRAVE)
+    e9:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+    e9:SetCountLimit(1,216666002)
+    e9:SetCost(c216666002.gupcost)
+    e9:SetTarget(c216666002.guptg)
+    e9:SetOperation(c216666002.gupop)
+    c:RegisterEffect(e9)
+end
+
+function c216666002.pncon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local seq=c:GetSequence()
+    local tc=Duel.GetFieldCard(tp,LOCATION_SZONE,13-seq)
+    return tc and tc:IsSetCard(0x10db) and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+end
+function c216666002.pntg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return false end
+    local tc1=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+    local tc2=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+    if chk==0 then 
+        return tc1 and tc2 and tc1:IsCanBeEffectTarget(e) and tc2:IsCanBeEffectTarget(e)
+    end
+    local g=Group.FromCards(tc1,tc2)
+    Duel.SetTargetCard(g)
+end
+function c216666002.pnop(e,tp,eg,ep,ev,re,r,rp)
+    local tc1=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+    local tc2=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+    if not tc1 or not tc2 then return end
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_SPSUMMON_PROC_G)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_BOTH_SIDE)
+    e1:SetRange(LOCATION_PZONE)
+    e1:SetCountLimit(1,10000000)
+    e1:SetCondition(c216666002.pendcon)
+    e1:SetOperation(c216666002.pendop)
+    e1:SetValue(SUMMON_TYPE_PENDULUM)
+    e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+    e1:SetLabel(tc2:GetFieldID())
+    tc1:RegisterEffect(e1)
+end
+function c216666002.pendfil(c,e,tp,lscale,rscale)
+    local lv=c:GetLevel()
+    return c:IsSetCard(0x10db) and c:IsLocation(LOCATION_GRAVE) and c:IsControler(tp) and lv>lscale and lv<rscale and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_PENDULUM,tp,false,false) and not c:IsForbidden()
+end
+function c216666002.pendcon(e,c,og)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    if c:GetSequence()~=6 then return false end
+    local rpz=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+    if rpz==nil then return false end
+    local lscale=c:GetLeftScale()
+    local rscale=rpz:GetRightScale()
+    if lscale>rscale then lscale,rscale=rscale,lscale end
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return false end
+    if og then
+        return og:IsExists(c216666002.pendfil,1,nil,e,tp,lscale,rscale)
+    else
+        return Duel.IsExistingMatchingCard(c216666002.pendfil,tp,LOCATION_GRAVE,0,1,nil,e,tp,lscale,rscale)
+    end
+end
+function c216666002.pendop(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
+    Duel.Hint(HINT_CARD,0,216666002)
+    local rpz=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+    local lscale=c:GetLeftScale()
+    local rscale=rpz:GetRightScale()
+    if lscale>rscale then lscale,rscale=rscale,lscale end
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if og then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=og:FilterSelect(tp,c216666002.pendfil,1,ft,nil,e,tp,lscale,rscale)
+        sg:Merge(g)
+    else
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,c216666002.pendfil,tp,LOCATION_GRAVE,0,1,ft,nil,e,tp,lscale,rscale)
+        sg:Merge(g)
+    end
+end
+
+function c216666002.uncon(e)
+    return e:GetHandler():IsStatus(STATUS_UNION)
+end
+function c216666002.filter(c)
+    return c:IsFaceup() and c:IsSetCard(0x10db) and c:GetUnionCount()==0
+end
+function c216666002.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c216666002.filter(chkc) and chkc~=e:GetHandler() end
+    if chk==0 then return e:GetHandler():GetFlagEffect(216666002)==0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+        and Duel.IsExistingTarget(c216666002.filter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    local g=Duel.SelectTarget(tp,c216666002.filter,tp,LOCATION_MZONE,0,1,1,e:GetHandler())
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+    e:GetHandler():RegisterFlagEffect(216666002,RESET_EVENT+0x7e0000+RESET_PHASE+PHASE_END,0,1)
+end
+function c216666002.eqop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+    if not tc:IsRelateToEffect(e) or not c216666002.filter(tc) then
+        Duel.SendtoGrave(c,REASON_EFFECT)
+        return
+    end
+    if not Duel.Equip(tp,c,tc,false) then return end
+    c:SetStatus(STATUS_UNION,true)
+end
+function c216666002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():GetFlagEffect(216666002)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+    e:GetHandler():RegisterFlagEffect(216666002,RESET_EVENT+0x7e0000+RESET_PHASE+PHASE_END,0,1)
+end
+function c216666002.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP_ATTACK)
+    end
+end
+
+function c216666002.gupcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+    Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function c216666002.gupfil(c)
+    return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function c216666002.guptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c216666002.gupfil(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(c216666002.gupfil,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    Duel.SelectTarget(tp,c216666002.gupfil,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c216666002.gupop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(600)
+        e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+    end
+end
