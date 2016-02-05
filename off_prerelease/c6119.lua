1e0589bef14451b31558c939d4c85505d32056c2
EerieCode <eeriecode@gmail.com>
2016-02-05 12:15:03 +0100

    Update c6119.lua

--- off_prerelease/c6119.lua
+++ off_prerelease/c6119.lua
@@ -12,6 +12,7 @@ function c6119.initial_effect(c)
   c:RegisterEffect(e1)
   --Send to grave
   local e2=Effect.CreateEffect(c)
+  e2:SetDescription(aux.Stringid(6119,0))
   e2:SetCategory(CATEGORY_TOGRAVE)
   e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
   e2:SetCode(EVENT_BATTLE_DESTROYING)
@@ -19,6 +20,18 @@ function c6119.initial_effect(c)
   e2:SetTarget(c6119.tgtg)
   e2:SetOperation(c6119.tgop)
   c:RegisterEffect(e2)
+  --Special Summon
+  local e3=Effect.CreateEffect(c)
+  e3:SetDescription(aux.Stringid(6119,1))
+  e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e3:SetType(EFFECT_TYPE_IGNITION)
+  e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+  e3:SetRange(LOCATION_MZONE)
+  e3:SetCountLimit(1,6119)
+  e3:SetCost(c6119.spcost)
+  e3:SetTarget(c6119.sptg)
+  e3:SetOperation(c6119.spop)
+  c:RegisterEffect(e3)
 end
 
 function c6119.lvcon(e)
@@ -37,5 +50,28 @@ function c6119.tgop(e,tp,eg,ep,ev,re,r,rp)
   local g=Duel.SelectMatchingCard(tp,c6119.tgfil,tp,LOCATION_DECK,0,1,1,nil)
   if g:GetCount()>0 then
     Duel.SendtoGrave(g,REASON_EFFECT)
+  end
+end
+
+function c6119.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsAbleToGraveAsCost,1,1,REASON_COST)
+end
+function c6119.spfil(c,e,tp)
+  return c:IsRace(RACE_DRAGON) and (c:GetLevel()==7 or c:GetLevel()==8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c6119.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c6119.spfil(chkc,e,tp) end
+  if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 and e:GetHandler():IsAbleToGrave() and Duel.IsExistingTarget(c6119.spfil,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+  local g=Duel.SelectTarget(tp,c6119.spfil,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,g:GetCount(),0,0)
+end
+function c6119.spop(e,tp,eg,ep,ev,re,r,rp)
+  if Duel.GetLocationCount(tp,LOCATION_MZONE)<=-1 then return end
+  local c=e:GetHandler()
+  local tc=Duel.GetFirstTarget()
+  if tc:IsRelateToEffect(e) and Duel.SendtoGrave(c,REASON_EFFECT)>0 then
+    Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
   end
 end
