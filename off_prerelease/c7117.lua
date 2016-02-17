ace6745bc24df9adfaea9a189ee0da897fdb5a79
EerieCode <eeriecode@gmail.com>
2016-02-17 15:08:45 +0100

    Update c7117.lua

--- off_prerelease/c7117.lua
+++ off_prerelease/c7117.lua
@@ -15,7 +15,7 @@ function c7117.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_HAND)
-	e2:SetOperation(c7117.spsreg)
+	e2:SetOperation(c7117.chainreg)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(7117,0))
@@ -30,17 +30,42 @@ function c7117.initial_effect(c)
 	e3:SetOperation(c7117.spsop)
 	c:RegisterEffect(e3)
   --Special Summon (DM)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(7117,0))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_CHAIN_SOLVING)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_NO_TURN_RESET+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCountLimit(1)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(c7117.spcon)
+	e4:SetCost(c7117.spcost)
+	e4:SetTarget(c7117.sptg)
+	e4:SetOperation(c7117.spop)
+	c:RegisterEffect(e4)
+  local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e5:SetCode(EVENT_CHAINING)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetOperation(c7117.chainreg2)
+	c:RegisterEffect(e5)
 end
 
-function c7117.spsreg(e,tp,eg,ep,ev,re,r,rp)
+function c7117.chainreg(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():GetFlagEffect(7117)==0 then
 		e:GetHandler():RegisterFlagEffect(7117,RESET_EVENT+0x1fc0000+RESET_CHAIN,0,1)
+	end
+end
+function c7117.chainreg2(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():GetFlagEffect(7117+1)==0 then
+		e:GetHandler():RegisterFlagEffect(7117+1,RESET_EVENT+0x1fc0000+RESET_CHAIN,0,1)
 	end
 end
 
 function c7117.spscon(e,tp,eg,ep,ev,re,r,rp)
 	local c=re:GetHandler()
-	return rp==tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsType(TYPE_SPELL+TYPE_TRAP) and Duel.GetTurnPlayer()~=tp and e:GetHandler():GetFlagEffect(1)>0
+	return rp==tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsType(TYPE_SPELL+TYPE_TRAP) and Duel.GetTurnPlayer()~=tp and e:GetHandler():GetFlagEffect(7117)>0
 end
 function c7117.spstg(e,tp,eg,ep,ev,re,r,rp,chk)
   local c=e:GetHandler()
@@ -52,4 +77,30 @@ function c7117.spsop(e,tp,eg,ep,ev,re,r,rp)
   if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
     Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
   end
+end
+
+function c7117.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=re:GetHandler()
+	return rp==tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsType(TYPE_SPELL+TYPE_TRAP) and e:GetHandler():GetFlagEffect(7117+1)>0
+end
+function c7117.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,7117)==0 end
+	Duel.RegisterFlagEffect(tp,7117,RESET_PHASE+PHASE_END,0,1)
+end
+function c7117.spfil(c,e,tp)
+	return c:IsCode(46986414) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c7117.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c7117.spfil(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(c7117.spfil,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c7117.spfil,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c7117.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
