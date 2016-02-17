254c24e6c1b5568d71bf3f322fcacf68208b22ba
EerieCode <eeriecode@gmail.com>
2016-02-17 18:18:53 +0100

    Update c7119.lua

--- off_prerelease/c7119.lua
+++ off_prerelease/c7119.lua
@@ -15,7 +15,24 @@ function c7119.initial_effect(c)
   c:RegisterEffect(e1)
   --Special Summon (Self)
   local e2=Effect.CreateEffect(c)
-  c:RegisterEffect(e2)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetOperation(aux.chainreg)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(7119,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_CHAIN_SOLVING)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,7119+1)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCondition(c7119.spscon)
+	e3:SetTarget(c7119.spstg)
+	e3:SetOperation(c7119.spsop)
+	c:RegisterEffect(e3)
 end
 
 function c7119.spcon(e,tp,eg,ep,ev,re,r,rp)
@@ -41,4 +58,26 @@ function c7119.spop(e,tp,eg,ep,ev,re,r,rp)
   if g:GetCount()>0 then
     Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
   end
+end
+
+function c7119.spscon(e,tp,eg,ep,ev,re,r,rp)
+	local c=re:GetHandler()
+	return rp==tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsType(TYPE_SPELL+TYPE_TRAP) and Duel.GetTurnPlayer()~=tp and e:GetHandler():GetFlagEffect(7119)>0
+end
+function c7119.spstg(e,tp,eg,ep,ev,re,r,rp,chk)
+  local c=e:GetHandler()
+  if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function c7119.spsop(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x47e0000)
+		e1:SetValue(LOCATION_REMOVED)
+		c:RegisterEffect(e1,true)
+	end
 end
