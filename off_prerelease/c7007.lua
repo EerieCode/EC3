389df27aa9e984a0d7543a64d4a79cd735417e32
EerieCode <eeriecode@gmail.com>
2016-02-17 13:36:00 +0100

    Update c7007.lua

--- off_prerelease/c7007.lua
+++ off_prerelease/c7007.lua
@@ -24,6 +24,16 @@ function c7007.initial_effect(c)
 	e2:SetOperation(c7007.thop)
 	c:RegisterEffect(e2)
   --Special Summon
+  local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(7007,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,7007)
+	e3:SetTarget(c7007.sptg)
+	e3:SetOperation(c7007.spop)
+	c:RegisterEffect(e3)
 end
 
 function c7007.valcon(e,re,r,rp)
@@ -43,4 +53,27 @@ function c7007.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
   local g=Duel.SelectTarget(tp,c7007.thfil,tp,LOCATION_GRAVE,0,1,1,nil)
   Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
   Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c7007.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and Duel.SendtoGrave(c,REASON_EFFECT)~=0 then
+		Duel.SendtoHand(tc,tp,REASON_EFFECT)
+	end
+end
+
+function c7007.spcfil(c)
+	return c:IsSetCard(0x9f) and c:IsPreviousLocation(LOCATION_HAND)
+end
+function c7007.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return eg:IsExists(c7007.spcfil,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function c7007.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
