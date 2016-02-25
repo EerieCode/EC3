758dc3d918e64c991eb5016f8f67244cc881b48d
EerieCode <eeriecode@gmail.com>
2016-02-25 11:14:55 +0100

    Update Performapal Bot-Eyes Lizard.lua

--- off_prerelease/Performapal Bot-Eyes Lizard.lua
+++ off_prerelease/Performapal Bot-Eyes Lizard.lua
@@ -1,3 +1,52 @@
 --Scripted by Eerie Code
 --Performapal Bot-Eyes Lizard
 function c7105.initial_effect(c)
+  local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(7105,0))
+  e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetCountLimit(1)
+  e1:SetCost(c7105.cost)
+  e1:SetOperation(c7105.op)
+  c:RegisterEffect(e1)
+end
+
+function c7105.cfilter(c)
+  return c:IsSetCard(0x99) and c:IsAbleToGraveAsCost()
+end
+function c7105.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(c7105.cfilter,tp,LOCATION_DECK,0,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local cg=Duel.SelectMatchingCard(tp,c7105.cfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(cg,REASON_COST)
+	e:SetLabel(cg:GetFirst():GetCode())
+end
+function c7105.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	e1:SetValue(e:GetLabel())
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(7105,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	e2:SetLabelObject(e1)
+	e2:SetOperation(c89312388.rstop)
+	c:RegisterEffect(e2)
+end
+function c7105.rstop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=e:GetLabelObject()
+	e1:Reset()
+	Duel.HintSelection(Group.FromCards(c))
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+end
