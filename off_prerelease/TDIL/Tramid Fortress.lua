151ff0acf50cf0cb70c41300a1bddf8c459e3f9e
EerieCode <eeriecode@gmail.com>
2016-04-06 13:09:52 +0200

    Update Tramid Fortress.lua

--- off_prerelease/TDIL/Tramid Fortress.lua
+++ off_prerelease/TDIL/Tramid Fortress.lua
@@ -1,2 +1,17 @@
 --Scripted by Eerie Code
 --Tramid Fortress
+function c7162.initial_effect(c)
+  --Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_DEFENCE)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_ROCK))
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
