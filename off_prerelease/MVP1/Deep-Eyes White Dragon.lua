692efcff502f6f358368fb36d6f2d52562e85348
EerieCode <eeriecode@gmail.com>
2016-04-19 10:52:50 +0200

    Update Deep-Eyes White Dragon.lua

--- off_prerelease/MVP1/Deep-Eyes White Dragon.lua
+++ off_prerelease/MVP1/Deep-Eyes White Dragon.lua
@@ -9,30 +9,65 @@ function c7205.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_DESTROYED)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c49460512.spcon)
-	e1:SetTarget(c49460512.sptg)
-	e1:SetOperation(c49460512.spop)
+	e1:SetCondition(c7205.spcon)
+	e1:SetTarget(c7205.sptg)
+	e1:SetOperation(c7205.spop)
 	c:RegisterEffect(e1)
+	--Change ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetTarget(c7205.atktg)
+	e2:SetOperation(c7205.atkop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
 end
 
-function c49460512.cfilter(c,tp)
+function c7205.cfilter(c,tp)
 	return c:IsSetCard(0xdd) and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
 		and c:IsPreviousPosition(POS_FACEUP) and c:IsReason(REASON_BATTLE) or (rp~=tp and c:IsReason(REASON_EFFECT))
 end
-function c49460512.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c49460512.cfilter,1,nil,tp)
+function c7205.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c7205.cfilter,1,nil,tp)
 end
-function c49460512.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c7205.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	local mg=Duel.GetMatchingGroup(Card.IsRace,tp,LOCATION_GRAVE,0,nil)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,mg:GetClassCount(Card.GetCode)*600)
 end
-function c49460512.spop(e,tp,eg,ep,ev,re,r,rp)
+function c7205.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
-	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLocation(LOCATION_HAND) then
-		Duel.SendtoGrave(c,REASON_RULE)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local mc=Duel.GetMatchingGroup(Card.IsRace,tp,LOCATION_GRAVE,0,nil):GetClassCount(Card.GetCode)
+		Duel.Damage(1-tp,600*mc,REASON_EFFECT)
+	else
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLocation(LOCATION_HAND) then
+			Duel.SendtoGrave(c,REASON_RULE)
+		end
+	end
+end
+
+function c7205.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and chkc:IsRace(RACE_DRAGON) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsRace,tp,LOCATION_GRAVE,0,1,nil,RACE_DRAGON) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,Card.IsRace,tp,LOCATION_GRAVE,0,1,1,nil,RACE_DRAGON)
+end
+function c7205.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(tc:GetAttack())
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e1)
 	end
 end
