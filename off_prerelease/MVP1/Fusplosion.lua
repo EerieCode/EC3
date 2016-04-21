0da28720ec9a623874f3f171217298ba7392111b
EerieCode <eeriecode@gmail.com>
2016-04-21 14:39:45 +0200

    Update Fusplosion.lua

--- off_prerelease/MVP1/Fusplosion.lua
+++ off_prerelease/MVP1/Fusplosion.lua
@@ -2,3 +2,48 @@
 --Fusplosion
 --Scripted by Eerie Code
 function c7209.initial_effect(c)
+  --Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(c7209.condition)
+	e1:SetTarget(c7209.target)
+	e1:SetOperation(c7209.activate)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(c7209.condition2)
+	e2:SetCost(c7209.cost)
+	c:RegisterEffect(e2)
+end
+
+function c7209.cfilter(c)
+	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_ONFIELD) and re:IsActiveType(TYPE_SPELL)
+end
+function c7209.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c7209.cfilter,1,nil)
+end
+function c7209.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) and chkc:IsDestructable() end
+  if chk==0 then return Duel.IsExistingTarget(Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+  local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c7209.activate(e,tp,eg,ep,ev,re,r,rp)
+  local tc=Duel.GetFirstTarget()
+  if tc:IsRelateToEffect(e) then
+    Duel.Destroy(tc,REASON_EFFECT)
+  end
+end
+
+function c7209.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return c7209.condition(e,tp,eg,ep,ev,re,r,rp) and aux.exccon(e)
+end
+function c7209.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
