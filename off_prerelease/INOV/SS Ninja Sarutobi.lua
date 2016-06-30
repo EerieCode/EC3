b8e6a7cbc5ebe5225dbbc62d75242c35bfdc0064
EerieCode <eeriecode@gmail.com>
2016-06-30 09:33:44 +0200

    Update SS Ninja Sarutobi.lua

--- off_prerelease/INOV/SS Ninja Sarutobi.lua
+++ off_prerelease/INOV/SS Ninja Sarutobi.lua
@@ -2,3 +2,53 @@
 --Superheavy Samurai Ninja Sarutobi
 --Scripted by Eerie Code
 function c7342.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_MACHINE),aux.NonTuner(Card.IsSetCard,0x9a),1)
+	c:EnableReviveLimit()
+	--defense attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DEFENSE_ATTACK)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(7342,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e2:SetCondition(c7342.con)
+	e2:SetTarget(c7342.tg)
+	e2:SetOperation(c7342.op)
+	c:RegisterEffect(e2)
+end
+
+function c7342.cfilter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function c7342.con(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(c494922.cfilter,tp,LOCATION_GRAVE,0,1,nil)
+end
+function c7342.fil(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsDestructable()
+end
+function c7342.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and c7342.fil(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c7342.fil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.SetTargetParam(500)
+	Duel.SetTargetPlayer(1-tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c7342.fil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+end
+function c7342.op(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChaininfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
+		Duel.Damage(p,d,REASON_EFFECT)
+	end
+end
