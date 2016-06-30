22e55826652a1ff3701c646ef4f8847bb32204ef
EerieCode <eeriecode@gmail.com>
2016-06-30 11:54:19 +0200

    Update Denglong.lua

--- off_prerelease/INOV/Denglong.lua
+++ off_prerelease/INOV/Denglong.lua
@@ -2,3 +2,97 @@
 --Denglong, Origin of the Yang Zing
 --Scripted by Eerie Code
 function c7348.initial_effect(c)
+	c:SetSPSummonOnce(7348)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(7348,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetTarget(c7348.thtg1)
+	e1:SetOperation(c7348.thop1)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(7348,1))
+	e2:SetCategory(CATEGORY_LVCHANGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(c7348.lvcost)
+	e2:SetOperation(c7348.lvop)
+	c:RegisterEffect(e2)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(7348,2))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCondition(c7348.thcon)
+	e3:SetTarget(c7348.thtg2)
+	e3:SetOperation(c7348.thop2)
+	c:RegisterEffect(e3)
+end
+
+function c7348.thfil1(c)
+	return c:IsSetCard(0x9e) and c:IsAbleToHand()
+end
+function c7348.thtg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c7348.thfil1,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
+end
+function c7348.thop1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c7348.thfil1,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+function c7348.lvcfil(c,lv)
+	return c:IsRace(RACE_WYRM) and c:GetLevel()~=lv and c:IsAbleToGraveAsCost()
+end
+function c7348.lvcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(c7348.lvcfil,tp,LOCATION_DECK,0,1,nil,c:GetLevel()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c7348.lvcfil,tp,LOCATION_DECK,0,1,1,nil,c:GetLevel())
+	local tc=g:GetFirst()
+	e:SetLabelObject(tc)
+	Duel.SendtoGrave(tc,REASON_EFFECT)
+end
+function c7348.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=e:GetLabelObject()
+	if c:IsFaceup() and c:IsRelateToEffect(e) and tc then
+		local lv=tc:GetLevel()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e1)
+	end
+end
+
+function c7348.thcon2(e,tp,eg,ep,ev,re,r,rp)
+	return e:Gethandler():IsPreviousPosition(POS_FACEUP)
+end
+function c7348.thfil2(c)
+	return c7348.thfil1(c) and c:IsType(TYPE_MONSTER)
+end
+function c7348.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c7348.thfil2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
+end
+function c7348.thop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c7348.thfil2,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
