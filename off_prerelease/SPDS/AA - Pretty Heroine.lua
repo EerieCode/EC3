4b7101973f451b3ae848e64df1782d57edfcc058
EerieCode <eeriecode@gmail.com>
2016-07-13 16:27:45 +0200

    Update AA - Pretty Heroine.lua

--- off_prerelease/SPDS/AA - Pretty Heroine.lua
+++ off_prerelease/SPDS/AA - Pretty Heroine.lua
@@ -2,3 +2,75 @@
 --Abyss Actor - Pretty Heroine
 --Scripted by Eerie Code
 function c7421.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BATTLE_DAMAGE)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(c7421.pcon)
+	e1:SetTarget(c7421.ptg)
+	e1:SetOperation(c7421.pop)
+	c:RegisterEffect(e1)
+end
+
+function c7421.aafil(c)
+	return (c:IsSetCard(0x10ee) or c:IsSetCard(0x120e))
+end
+function c7421.asfil(c)
+	return (c:IsSetCard(0x20ee) or c:IsSetCard(0x220e))
+end
+
+function c7421.pcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	return ep==tp and a:IsControler(1-tp)
+end
+function c7421.thfil(c,dam)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c7421.aafil(c) and c:IsAttackBelow(dam) and c:IsAbleToHand()
+end
+function c7421.ptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.GetAttacker()
+	local dam=ev
+	local b1=a:IsRelateToBattle()
+	local b2=Duel.IsExistingMatchingCard(c7421.thfil,tp,LOCATION_EXTRA,0,1,nil,dam)
+	if chk==0 then return b1 or b2 end
+	local opt=0
+	if b1 and b2 then
+		opt=Duel.SelectOption(tp,aux.Stringid(7421,0),aux.Stringid(7421,1))
+	elseif b1 then
+		opt=Duel.SelectOption(tp,aux.Stringid(7421,0))
+	else
+		opt=Duel.SelectOption(tp,aux.Stringid(7421,1))+1
+	end
+	e:SetLabel(opt)
+	if opt==0 then
+		e:SetCategory(CATEGORY_ATKCHANGE)
+	else
+		e:SetCategory(CATEGORY_TOHAND)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
+	end
+end
+function c7421.pop(e,tp,eg,ep,ev,re,r,rp)
+	local opt=e:GetLabel()
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if opt==0 then
+		local a=Duel.GetAttacker()
+		if a:IsRelateToBattle() then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(-ev)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
+			a:RegisterEffect(e1)
+		end
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,c7421.thfil,tp,LOCATION_EXTRA,0,1,1,nil,ev)
+		if g:GetCount()>0 then
+			Duel.SendtoHand(g,tp,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
