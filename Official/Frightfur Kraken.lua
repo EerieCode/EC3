d4020e398b686c641d8247fdcb254e31a482d348
EerieCode <eeriecode@gmail.com>
2016-09-16 14:54:57 +0200

    Update Frightfur Kraken.lua

--- Official/Frightfur Kraken.lua
+++ Official/Frightfur Kraken.lua
@@ -2,9 +2,9 @@
 --Frightfur Kraken
 --Scripted by Eerie Code
 function c7620.initial_effect(c)
-  --Fusion Materials
-  c:EnableReviveLimit()
-  aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,SET_EDGEIMP),aux.FilterBoolFunction(Card.IsFusionSetCard,SET_FLUFFAL),true)
+	--Fusion Materials
+	c:EnableReviveLimit()
+	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,SET_EDGEIMP),aux.FilterBoolFunction(Card.IsFusionSetCard,SET_FLUFFAL),true)
 	--tograve
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(7620,0))
@@ -17,6 +17,21 @@ function c7620.initial_effect(c)
 	e1:SetTarget(c7620.tgtg)
 	e1:SetOperation(c7620.tgop)
 	c:RegisterEffect(e1)
+	--extra att
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EXTRA_ATTACK)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--pos
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(7620,0))
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e3:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e3:SetCondition(c7620.poscon)
+	e3:SetOperation(c7620.posop)
+	c:RegisterEffect(e3)
 end
 
 function c7620.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -29,16 +44,25 @@ function c7620.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:GetHandler():RegisterEffect(e1)
 end
 function c7620.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-  if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToGrave() end
-  if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToGrave,tp,0,LOCATION_MZONE,1,nil) end
-  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-  local g=Duel.SelectTarget(tp,Card.IsAbleToGrave,tp,0,LOCATION_MZONE,1,1,nil)
-  Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToGrave() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToGrave,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToGrave,tp,0,LOCATION_MZONE,1,1,nil)
+  	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
 end
 function c7620.tgop(e,tp,eg,ep,ev,re,r,rp)
-  local tc=Duel.GetFirstTarget()
-  if tc:IsRelateToEffect(e) then
-    Duel.SendtoGrave(tc,REASON_EFFECT)
-  end
+  	local tc=Duel.GetFirstTarget()
+  	if tc:IsRelateToEffect(e) then
+    		Duel.SendtoGrave(tc,REASON_EFFECT)
+  	end
 end
 
+function c7620.poscon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetBattledGroupCount()>0 and c:IsAttackPos()
+end
+function c7620.posop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.ChangePosition(e:GetHandler(),POS_FACEUP_DEFENSE)
+	end
+end
