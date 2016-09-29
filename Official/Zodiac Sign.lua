db7499a20f1513a4cade5175bc3340fd319b7bda
EerieCode <eeriecode@gmail.com>
2016-09-29 10:04:14 +0200

    Update Zodiac Sign.lua

--- Official/Zodiac Sign.lua
+++ Official/Zodiac Sign.lua
@@ -1,3 +1,37 @@
+--ゾディアックＳ
 --Zodiac Sign
 --Scripted by Eerie code
 function c7558.initial_effect(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--atk&def
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetValue(300)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xf2))
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+	--at limit
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e4:SetValue(c7558.atlimit)
+	c:RegisterEffect(e4)
+end
+
+function c7558.atkfil(c,atk)
+	return c:IsFaceup() and c:IsRace(RACE_BEASTWARRIOR) and c:GetAttack()>atk
+end
+function c7558.atlimit(e,c)
+	return c:IsFaceup() and c:IsRace(RACE_BEASTWARRIOR) and not Duel.IsExistingMatchingCard(c7558.atkfil,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,c,c:GetAttack())
+end
