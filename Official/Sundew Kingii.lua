b7008bb3f1d3390ffd968c135e4a4a71a8705422
EerieCode <eeriecode@gmail.com>
2016-09-30 14:25:44 +0200

    Create Sundew Kingii.lua

---
+++ Official/Sundew Kingii.lua
@@ -0,0 +1,5 @@
+--捕食植物サンデウ・キンジー
+--Predator Plant Sundew Kingii
+--Scripted by Eerie Code
+--The 'fusattribute' effect is temporary, requires a core update to work properly
+function c7605.initial_effect(c)
