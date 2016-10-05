68507e24d991b3d706dd5ead21d78103dc6b33b4
EerieCode <eeriecode@gmail.com>
2016-10-05 14:59:29 +0200

    Update Card Regen.lua

--- Official/Card Regen.lua
+++ Official/Card Regen.lua
@@ -2,3 +2,40 @@
 --Card Regeneration
 --Scripted by Eerie Code
 function c7557.initial_effect(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(c7557.tg)
+	e1:SetOperation(c7557.op)
+	c:RegisterEffect(e1)
+end
+
+function c7557.fil(c)
+	return c:IsSetCard(SET_CARDIAN) and c:IsAbleToHand()
+end
+function c7557.spfil(c,e,tp)
+	return c:IsSetCard(SET_CARDIAN) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function c7557.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c7557.fil(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c7557.fil,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c7557.fil,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c7557.op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+		local g=Duel.GetMatchingGroup(c7557.spfil,tp,LOCATION_HAND,0,nil,e,tp)
+		if g:GetCount()>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(7557,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=g:Select(tp,1,1,nil)
+			Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)
+		end
+	end
+end
+	
