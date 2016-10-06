2436602f82ee6a65932cc5dabdecc4b78798ef50
EerieCode <eeriecode@gmail.com>
2016-10-06 10:45:07 +0200

    Update Elysion.lua

--- Official/Elysion.lua
+++ Official/Elysion.lua
@@ -2,3 +2,11 @@
 --Elysion the Eidolon Beast
 --Scripted by Eerie Code
 function c7633.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xf4),c7633.mat_fil,false)
+	
+end
+
+function c7633.mat_fil(c)
+	return c:GetPreviousLocation()==LOCATION_EXTRA
+end
