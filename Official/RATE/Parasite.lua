2e21cf32af31cf60f65138112130d78498c2152f
EerieCode <eeriecode@gmail.com>
2016-10-06 18:51:15 +0200

    Update Parasite.lua

--- Official/RATE/Parasite.lua
+++ Official/RATE/Parasite.lua
@@ -2,3 +2,24 @@
 --Parasite Fusioner
 --Scripted by Eerie Code
 function c7509.initial_effect(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(7509,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetTarget(c7509.tg)
+	e1:SetOperation(c7509.op)
+	c:RegisterEffect(e1)
+	--fusion substitute
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_FUSION_SUBSTITUTE)
+	e2:SetCondition(c7509.subcon)
+	c:RegisterEffect(e2)
+end
+
+function c7509.subcon(e)
+	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
+end
