0f1162100c13e61214055e369e8672e5987f85eb
EerieCode <eeriecode@gmail.com>
2016-10-07 09:47:46 +0200

    Update Laughmaker.lua

--- Official/RATE/Laughmaker.lua
+++ Official/RATE/Laughmaker.lua
@@ -2,3 +2,95 @@
 --Performapal Laughmaker
 --Scripted by Eerie Code
 function c7504.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(7504,0))
+	e1:SetCategory(CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c7504.rccon)
+	e1:SetTarget(c7504.rctg)
+	e1:SetOperation(c7504.rcop)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(7504,1))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetCountLimit(1,7504)
+	e2:SetCondition(c7504.atkcon)
+	e2:SetOperation(c7504.atkop)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCountLimit(1,7504)
+	e3:SetCondition(c7504.spcon)
+	e3:SetTarget(c7504.sptg)
+	e3:SetOperation(c7504.spop)
+	c:RegisterEffect(e3)
+end
+
+function c7504.rcfil(c)
+	return c:IsFaceup() and c:GetAttack()>c:GetBaseAttack()
+end
+function c7504.rccon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c7504.rcfil,tp,0,LOCATION_MZONE,1,nil)
+end
+function c7504.rctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1000)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,1000)
+end
+function c7504.rcop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end
+
+function c7504.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return c7504.rcfil(e:GetHandler()) or Duel.IsExistingMatchingCard(c7504.rcfil,tp,0,LOCATION_MZONE,1,nil)
+end
+function c7504.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToBattle() or c:IsFacedown() then return end
+	local ct=Duel.GetMatchingGroupCount(c7504.rcfil,tp,0,LOCATION_MZONE,nil)
+	if c7504.rcfil(c) then ct=ct+1 end
+	if ct>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(1000*ct)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+		c:RegisterEffect(e1)
+	end
+end
+
+function c7504.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousAttackOnField()>c:GetBaseAttack()
+end
+function c7504.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp)
+		and chkc:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsExistingTarget(Card.IsCanBeSpecialSummoned,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,Card.IsCanBeSpecialSummoned,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c7504.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
