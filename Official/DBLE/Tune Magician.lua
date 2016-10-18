7133befebe75b7fa074ee7e53bd42db5e52ca030
EerieCode <eeriecode@gmail.com>
2016-10-18 11:24:59 +0200

    Update Tune Magician.lua

--- Official/DBLE/Tune Magician.lua
+++ Official/DBLE/Tune Magician.lua
@@ -1,6 +1,7 @@
 --調弦の魔術師
 --Tune Magician
 --Scripted by Eerie Code
+--Incomplete effect
 function c7652.initial_effect(c)
 	aux.EnablePendulumAttribute(c)
 	--
@@ -22,6 +23,17 @@ function c7652.initial_effect(c)
 	e3:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e3:SetValue(aux.FALSE)
 	c:RegisterEffect(e3)
+	--
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(7652,0))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetCountLimit(1,7652)
+	e4:SetCondition(c7652.spcon)
+	e4:SetTarget(c7652.sptg)
+	e4:SetOperation(c7652.spop)
+	c:RegisterEffect(e4)
 end
 
 function c7652.atkfil(c)
@@ -29,4 +41,67 @@ function c7652.atkfil(c)
 end
 function c7652.atkval(e,c)
 	return Duel.GetMatchingGroupCount(c7652.atkfil,tp,LOCATION_EXTRA,0,nil)*100
+end
+
+function c7652.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_PENDULUM) and c:IsPreviousLocation(LOCATION_HAND)
+end
+function c7652.spfil(c,e,tp)
+	return c:IsType(TYPE_PENDULUM) and c:IsSetCard(SET_MAGICIAN) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function c7652.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c7652.spfil,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SpecialSummon(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c7652.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c7652.spfil,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+		local e0=Effect.CreateEffect(c)
+		e0:SetType(EFFECT_TYPE_SINGLE)
+		e0:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e0:SetReset(RESET_EVENT+0x47e0000)
+		e0:SetValue(LOCATION_REMOVED)
+		tc:RegisterEffect(e0)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e2:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e2)
+	end
+	Duel.SpecialSummonComplete()
+end
+
+--Synchro Limit
+c7652.tuner_filter=function(c) return c:IsType(TYPE_PENDULUM) and c:IsSetCard(SET_MAGICIAN) end
+function c7652.synfilter(c,syncard,tuner,f)
+	return c:IsFaceup() and c:IsNotTuner() and c:IsCanBeSynchroMaterial(syncard,tuner) and c:IsSetCard(SET_MAGICIAN) and (f==nil or f(c))
+end
+function c7652.syntg(e,syncard,f,minc,maxc)
+	local c=e:GetHandler()
+	local lv=syncard:GetLevel()-c:GetLevel()
+	if lv<=0 then return false end
+	local g=Duel.GetMatchingGroup(c7652.synfilter,syncard:GetControler(),LOCATION_MZONE,LOCATION_MZONE,c,syncard,c,f)
+	local res=g:CheckWithSumEqual(Card.GetSynchroLevel,lv,minc,maxc,syncard)
+	return res
+end
+function c7652.synop(e,tp,eg,ep,ev,re,r,rp,syncard,f,minc,maxc)
+	local c=e:GetHandler()
+	local lv=syncard:GetLevel()-c:GetLevel()
+	local g=Duel.GetMatchingGroup(c7652.synfilter,syncard:GetControler(),LOCATION_MZONE,LOCATION_MZONE,c,syncard,c,f)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
+	local sg=g:SelectWithSumEqual(tp,Card.GetSynchroLevel,lv,minc,maxc,syncard)
+	Duel.SetSynchroMaterial(sg)
 end
