23cd865945a9662b72b9f6c2e1ebb9069b872083
EerieCode <eeriecode@gmail.com>
2016-10-28 11:38:52 +0200

    Update Hydra.lua

--- Official/SPFE/Hydra.lua
+++ Official/SPFE/Hydra.lua
@@ -12,6 +12,19 @@ function c7601.initial_effect(c)
 	e1:SetCondition(c7601.hspcon)
 	e1:SetOperation(c7601.hspop)
 	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(7601,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetCountLimit(1,7601+1)
+	e2:SetCost(c7601.atkcost)
+	e2:SetTarget(c7601.atktg)
+	e2:SetOperation(c7601.atkop)
+	c:RegisterEffect(e2)
 end
 
 function c7601.hspfilter(c)
@@ -39,4 +52,31 @@ function c7601.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 		g=Duel.SelectMatchingCard(tp,c7601.hspfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	end
 	Duel.Release(g,REASON_COST)
+end
+
+function c7601.atkcfil(c)
+	return c:IsSetCard(0x10f3) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+function c7601.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c7601.atkcfil,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c7601.atkcfil,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c7601.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function c7601.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-500)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+	end
 end
