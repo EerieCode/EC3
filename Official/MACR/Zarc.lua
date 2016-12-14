3332ca25e2982ca5f8390c34eba39ba8c3c2ce8c
EerieCode <eeriecode@gmail.com>
2016-12-14 12:00:17 +0100

    Update Zarc.lua

--- Official/MACR/Zarc.lua
+++ Official/MACR/Zarc.lua
@@ -3,19 +3,76 @@
 --Scripted by Eerie Code
 function c100912039.initial_effect(c)
 	c:EnableReviveLimit()
+	--fusion procedure to be added
+	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_FUSION_MATERIAL)
-	e1:SetCondition(c100912039.fuscon)
-	e1:SetOperation(c100912039.fusop)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.fuslimit)
 	c:RegisterEffect(e1)
+	--act limit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetValue(c100912039.limval)
+	c:RegisterEffect(e2)
+	--destroy drawn
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100912039,0))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_HAND)
+	e3:SetRange(LOCATION_PZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c100912039.ddcon)
+	e3:SetTarget(c100912039.ddtg)
+	e3:SetOperation(c100912039.ddop)
+	c:RegisterEffect(e3)
+	--destroy all
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100912039,1))
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetTarget(c100912039.destg)
+	e4:SetOperation(c100912039.desop)
+	c:RegisterEffect(e4)
 end
-function c100912039.fusfil(c,t)
-	return c:IsRace(RACE_DRAGON) and c:IsType(t)
+function c100912039.limval(e,re,rp)
+	local rc=re:GetHandler()
+	return re:IsActiveType(TYPE_MONSTER) and rc:IsType(TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ) and not rc:IsImmuneToEffect(e)
 end
-function c100912039.fuscon(e,g,gc,chkfnf)
-	if g==nil then return false end
-	local chkf=bit.band(chkfnf,0xff)
-	local mg=g:Filter(Card.IsCanBeFusionMaterial,nil,e:GetHandler())
+function c100912039.ddcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DRAW
+end
+function c100912039.ddfilter(c,tp)
+	return c:IsControler(1-tp) and c:IsPreviousLocation(LOCATION_DECK)
+end
+function c100912039.ddtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=nil
+	if eg then g=eg:Filter(c100912039.ddfilter,nil,tp) end
+	if chk==0 then return g and g:GetCount()>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function c100912039.ddop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=eg:Filter(c100912039.ddfilter,nil,tp)
+	if g:GetCount()>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+function c100912039.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function c100912039.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	if g:GetCount()>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
 end
