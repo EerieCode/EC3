d52fb95a7268c8ecb0a911bb8ffd3f6f34ee3f69
EerieCode <eeriecode@gmail.com>
2017-03-17 15:20:59 +0100

    Update Eraser.lua

--- Official/COTD/Eraser.lua
+++ Official/COTD/Eraser.lua
@@ -12,6 +12,16 @@ function c101001072.initial_effect(c)
 	e1:SetCost(c101001072.cost)
 	e1:SetTarget(c101001072.target)
 	e1:SetOperation(c101001072.activate)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCondition(c101001072.spcon)
+	e2:SetTarget(c101001072.sptg)
+	e2:SetOperation(c101001072.spop)
+	c:RegisterEffect(e2)
 end
 function c101001072.filter1(c,tp)
 	return c:IsFaceup() and c:IsSetCard(0x38) 
@@ -43,5 +53,25 @@ function c101001072.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
 	if g:GetCount()>0 then
 		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function c101001072.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_DECK) and re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0x38) 
+		and bit.band(r,REASON_EFFECT)~=0
+end
+function c101001072.spfilter(c,e,tp)
+	return c:IsSetCard(0x38) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101001072.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101001072.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c101001072.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101001072.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
