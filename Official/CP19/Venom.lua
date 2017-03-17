f4641e90cd3cd4eeb5d0812e158fec43e1368023
EerieCode <eeriecode@gmail.com>
2017-03-17 11:01:52 +0100

    Update Venom.lua

--- Official/CP19/Venom.lua
+++ Official/CP19/Venom.lua
@@ -2,3 +2,56 @@
 --Odd-Eyes Venom Dragon
 --Scripted by Eerie Code
 function c100217006.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.EnablePendulumAttribute(c,false)
+	aux.AddFusionProcFun2(c,c100217006.matfilter,aux.FilterBoolFunction(Card.IsFusionSetCard,0x99),true)
+	--spsummon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(c100217006.splimit)
+	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100217006,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c100217006.atktg)
+	e2:SetOperation(c100217006.atkop)
+	c:RegisterEffect(e2)
+end
+function c100217006.matfilter(c)
+	return c:IsFusionCode(41209827) or c:IsFusionSetCard(0x1050)
+end
+function c100217006.splimit(e,se,sp,st)
+	return not e:GetHandler():IsLocation(LOCATION_EXTRA)
+		or bit.band(st,SUMMON_TYPE_FUSION)~=SUMMON_TYPE_FUSION
+		or bit.band(st,SUMMON_TYPE_PENDULUM)~=SUMMON_TYPE_PENDULUM
+end
+function c100217006.atkfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION)
+end
+function c100217006.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c100217006.atkfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100217006.atkfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c100217006.atkfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c100217006.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	if c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) and ct>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(1000*ct)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
