d179282a15d1a80a36062df4e295948d1e7bd04d
EerieCode <eeriecode@gmail.com>
2017-04-06 18:06:40 +0200

    Update Meeting.lua

--- Official/COTD/Meeting.lua
+++ Official/COTD/Meeting.lua
@@ -2,3 +2,21 @@
 --Chance Meeting with the Star Relic
 --Scripted by Eerie Code
 function c101001102.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--boost
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1fd))
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetValue(300)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+end
