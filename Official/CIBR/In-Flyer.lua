d951357e0f15bbba5ceb0675f71a200518aeaa02
EerieCode <eeriecode@gmail.com>
2017-06-15 09:33:19 +0200

    Create In-Flyer.lua

--- Official/CIBR/In-Flyer.lua
+++ Official/CIBR/In-Flyer.lua
@@ -1,7 +1,6 @@
 --リンク・インフライヤー
 --Link In-Flyer
 --Scripted by Eerie Code
---incomplete
 function c101002003.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
@@ -12,11 +11,16 @@ function c101002003.initial_effect(c)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,101002003)
 	e1:SetCondition(c101002003.spcon)
+	e1:SetOperation(c101002003.spop)
 	c:RegisterEffect(e1)
 end
 function c101002003.spcon(e,c)
 	if c==nil then return true end
-  local tp=e:GetHandlerPlayer()
+	local tp=e:GetHandlerPlayer()
 	local zone=Duel.GetLinkedZone(tp)
 	return zone~=0 and c:IsCanBeSpecialSummon(e,0,tp,false,false,POS_FACEUP,tp,zone)
+end
+function c101002003.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local zone=Duel.GetLinkedZone(tp)
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP,zone)
 end
