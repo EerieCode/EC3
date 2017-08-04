a9cf2fb167c2ad4e4222aeebd005dbefb82dbfd6
EerieCode <eeriecode@gmail.com>
2017-08-04 15:19:20 +0200

    Update Lampla.lua

--- Official/DBSW/Weather/Lampla.lua
+++ Official/DBSW/Weather/Lampla.lua
@@ -2,24 +2,67 @@
 --Polar Weathery Lampla
 --Scripted by Eerie Code
 function c100419034.initial_effect(c)
-	--spsummon
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetCode(EVENT_REMOVE)
-	e3:SetOperation(c100419034.spreg)
+	--place
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100419034,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(c100419034.tftg)
+	e1:SetOperation(c100419034.tfop)
+	c:RegisterEffect(e1)
+	--Untargetable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetTargetRange(LOCATION_ONFIELD,0)
+	e2:SetTarget(c100419034.immtg)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+	--Indes
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetValue(c100419034.tgvalue)
 	c:RegisterEffect(e3)
+	--spsummon
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(100419034,1))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetRange(LOCATION_REMOVED)
-	e4:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e4:SetCondition(c100419034.spcon)
-	e4:SetTarget(c100419034.sptg)
-	e4:SetOperation(c100419034.spop)
-	e4:SetLabelObject(e3)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EVENT_REMOVE)
+	e4:SetOperation(c100419034.spreg)
 	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(100419034,1))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetRange(LOCATION_REMOVED)
+	e5:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e5:SetCondition(c100419034.spcon)
+	e5:SetTarget(c100419034.sptg)
+	e5:SetOperation(c100419034.spop)
+	e5:SetLabelObject(e4)
+	c:RegisterEffect(e5)
+end
+function c100419034.tffilter(c,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSetCard(0x207) and not c:IsForbidden() and c:CheckUniqueOnField(tp)
+end
+function c100419034.tftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(c100419034.tffilter,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,nil,tp) end
+end
+function c100419034.tfop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100419034.tffilter),tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+	if tc then
+		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	end
+end
+function c100419034.immtg(e,c)
+	return c:IsSetCard(0x207) and c:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function c100419034.tgvalue(e,re,rp)
+	return rp~=e:GetHandlerPlayer()
 end
 function c100419034.spreg(e,tp,eg,ep,ev,re,r,rp)
 	if not re then return end
