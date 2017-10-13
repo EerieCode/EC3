fc57ee28579e4549c043890c0d353c894f1a04ad
EerieCode <eeriecode@gmail.com>
2017-10-13 09:57:10 +0200

    Update Borrel.lua

--- Official/EXFO/Borrel.lua
+++ Official/EXFO/Borrel.lua
@@ -32,7 +32,7 @@ function c101003068.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
 function c101003068.eqlimit(e,c)
-	return c:GetControler()==e:GetHandlerPlayer() and c:IsType(TYPE_LINK) and c:IsBorrel()
+	return c:GetControler()==e:GetHandlerPlayer() and c:IsType(TYPE_LINK) and c:IsSetCard(0x20d)
 end
 function c101003068.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -43,62 +43,42 @@ function c101003068.activate(e,tp,eg,ep,ev,re,r,rp)
 		c:CancelToGrave()
 		--
 		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_ADJUST)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetRange(LOCATION_SZONE)
-		e1:SetTarget(c101003068.adjtg)
-		e1:SetOperation(c101003068.adjop)
+		e1:SetValue(c101003068.eqlimit)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
 		c:RegisterEffect(e1)
+		--
 		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_EQUIP_LIMIT)
-		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e2:SetValue(c101003068.eqlimit)
+		e2:SetDescription(aux.Stringid(101003068,0))
+		e2:SetType(EFFECT_TYPE_QUICK_O)
+		e2:SetCode(EVENT_FREE_CHAIN)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 		e2:SetReset(RESET_EVENT+0x1fe0000)
-		c:RegisterEffect(e2)
+		e2:SetCountLimit(1)
+		e2:SetTarget(c101003068.indtg)
+		e2:SetOperation(c101003068.intop)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+		e3:SetRange(LOCATION_SZONE)
+		e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+		e3:SetTarget(c101003068.eftg)
+		e3:SetLabelObject(e2)
+		c:RegisterEffect(e3)
 	end
 end
-function c101003068.adjfilter(c)
-	return c:GetFlagEffect(101003068)==0
+function c101003068.eftg(e,c)
+	return e:GetHandler():GetEquipTarget()==c
 end
-function c101003068.adjtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101003068.adjfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-end
-function c101003068.adjop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(c101003068.adjfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if g:GetCount()>0 then
-		for tc in aux.Next(g) do
-			tc:RegisterFlagEffect(101003068,RESET_EVENT+0x1fe0000,0,1)
-			local e1=Effect.CreateEffect(c)
-			e1:SetDescription(aux.Stringid(16617334,1))
-			e1:SetType(EFFECT_TYPE_QUICK_O)
-			e1:SetCode(EVENT_FREE_CHAIN)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-			e1:SetLabelObject(c)
-			e1:SetReset(RESET_EVENT+RESET_EVENT+0x1fe0000)
-			e1:SetCountLimit(1)
-			e1:SetCondition(c101003068.effcon)
-			e1:SetTarget(c101003068.efftg)
-			e1:SetOperation(c101003068.effop)
-			tc:RegisterEffect(e1)
-		end
-	end
-end
-function c101003068.effcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetLabelObject()
-	local ec=e:GetHandler()
-	return c:GetEquipTarget()==ec
-end
-function c101003068.efftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c101003068.indtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function c101003068.effop(e,tp,eg,ep,ev,re,r,rp)
+function c101003068.indop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
